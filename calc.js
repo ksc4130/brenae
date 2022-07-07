eebdc126f8edbbf0c992b4bf03aae215769b00ae
BrenaeH <BrenaeH@Brenaes-MacBook-Pro.local>
2022-07-06 22:25:06 -0400

    First addition fuction

---
+++ calc.js
@@ -0,0 +1,27 @@
+bbash-3.2$ num1=23
+bash-3.2$ num2=47
+bash-3.2$ sum=$((num1+num2))
+bash-3.2$ echo $sum
+70
+bash-3.2$ read -p "Please enter an Integer : " num1
+Please enter an Integer : 23
+bash-3.2$ read -p "Please enter another Integer : " num2
+Please enter another Integer : 47
+bash-3.2$ sum=$((num1+num2))
+bash-3.2$ echo $sum
+70
+bash-3.2$ pwd
+ash-3.2$ num1=23
+bash-3.2$ num2=47
+bash-3.2$ sum=$((num1+num2))
+bash-3.2$ echo $sum
+70
+bash-3.2$ read -p "Please enter an Integer : " num1
+Please enter an Integer : 23
+bash-3.2$ read -p "Please enter another Integer : " num2
+Please enter another Integer : 47
+bash-3.2$ sum=$((num1+num2))
+bash-3.2$ echo $sum
+70
+bash-3.2$ pwd
+
