7d8ec66761532758afdb8c587c9affa43c7d1d3b
Brenae Harris <brenaeharris01@gmail.com>
2022-07-25 20:39:13 -0400

    Calculator

--- calc.js
+++ calc.js
@@ -1,27 +1,7 @@
-bbash-3.2$ num1=23
-bash-3.2$ num2=47
-bash-3.2$ sum=$((num1+num2))
-bash-3.2$ echo $sum
-70
-bash-3.2$ read -p "Please enter an Integer : " num1
-Please enter an Integer : 23
-bash-3.2$ read -p "Please enter another Integer : " num2
-Please enter another Integer : 47
-bash-3.2$ sum=$((num1+num2))
-bash-3.2$ echo $sum
-70
-bash-3.2$ pwd
-ash-3.2$ num1=23
-bash-3.2$ num2=47
-bash-3.2$ sum=$((num1+num2))
-bash-3.2$ echo $sum
-70
-bash-3.2$ read -p "Please enter an Integer : " num1
-Please enter an Integer : 23
-bash-3.2$ read -p "Please enter another Integer : " num2
-Please enter another Integer : 47
-bash-3.2$ sum=$((num1+num2))
-bash-3.2$ echo $sum
-70
-bash-3.2$ pwd
+var num1 = parseInt(prompt("Enter first number:"));
+var num2 = parseInt(prompt("Enter second number:"));
+var sum = num1 + num2;
+console. log("The sum of" + num1 + "and" + num2 + "is: " + sum);
 
+git config --global user.email "brenaeharris01@gmail.com"
+git config --global user.name "Brenae Harris"
